64e5be5115ff44103db077c273fff1ffd2e9bf2d
Gyuris Martin <h260480@stud.u-szeged.hu>
2024-04-19 18:27:51 +0200

    quick fix 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,7 +39,6 @@ import { environment } from '../environments/environment';
     FlexLayoutModule,
     MatListModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    // provideFirebaseApp(() => initializeApp({"projectId":"online-koncertjegy","appId":"1:599167310456:web:605183f5e9782316b4eb8d","storageBucket":"online-koncertjegy.appspot.com","apiKey":"AIzaSyB82oUAG1VFhZx684nM0ONXWD0biSPMcUQ","authDomain":"online-koncertjegy.firebaseapp.com","messagingSenderId":"599167310456","measurementId":"G-NWZQ16NVX2"})),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
