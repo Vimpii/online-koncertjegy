593f9730192d7ff37609120970ef4d58d6022fc3
Gyuris Martin <h260480@stud.u-szeged.hu>
2024-04-19 18:19:51 +0200

    quick fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -37,15 +38,7 @@ import { AngularFireModule } from '@angular/fire/compat';
     MatIconModule,
     FlexLayoutModule,
     MatListModule,
-    AngularFireModule.initializeApp({
-      projectId: 'online-koncertjegy',
-      appId: '1:599167310456:web:605183f5e9782316b4eb8d',
-      storageBucket: 'online-koncertjegy.appspot.com',
-      apiKey: 'AIzaSyB82oUAG1VFhZx684nM0ONXWD0biSPMcUQ',
-      authDomain: 'online-koncertjegy.firebaseapp.com',
-      messagingSenderId: '599167310456',
-      measurementId: 'G-NWZQ16NVX2',
-    }),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     // provideFirebaseApp(() => initializeApp({"projectId":"online-koncertjegy","appId":"1:599167310456:web:605183f5e9782316b4eb8d","storageBucket":"online-koncertjegy.appspot.com","apiKey":"AIzaSyB82oUAG1VFhZx684nM0ONXWD0biSPMcUQ","authDomain":"online-koncertjegy.firebaseapp.com","messagingSenderId":"599167310456","measurementId":"G-NWZQ16NVX2"})),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    projectId: 'online-koncertjegy',
+    appId: '1:599167310456:web:605183f5e9782316b4eb8d',
+    storageBucket: 'online-koncertjegy.appspot.com',
+    apiKey: 'AIzaSyB82oUAG1VFhZx684nM0ONXWD0biSPMcUQ',
+    authDomain: 'online-koncertjegy.firebaseapp.com',
+    messagingSenderId: '599167310456',
+    measurementId: 'G-NWZQ16NVX2',
+  },
+};
